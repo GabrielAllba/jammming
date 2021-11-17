8b09d70156c1475f6efef6129ce7c18b12a301ef
Gabriel Allba <rielgasy9@gmail.com>
2021-11-17 12:33:42 +0700

    add utils and other components

--- src/Components/SearchResult/App.js
+++ src/Components/SearchResult/App.js
@@ -3,12 +3,12 @@ import './style.css'
 import {TrackList} from '../TrackList/App'
 export class SearchResult extends React.Component{
     render(){
-       console.log(this.props)
-       console.log('hello')
         return(
             <div class="SearchResults">
                 <h2>Results</h2>
-                <TrackList tracks={this.props.searchResults}/>
+                <TrackList tracks={this.props.searchResults} 
+                onAdd={this.props.onAdd}
+                isRemoval={false}/>
             </div>
         )
     }
--- src/Components/TrackList/App.js
+++ src/Components/TrackList/App.js
@@ -4,14 +4,16 @@ import {Track}  from '../Track/App'
 
 export class TrackList extends React.Component{
     render(){
+        console.log('hello theresdfsdf riel')
+        console.log(this.props)
+        
         return(
             <div className="TrackList">
-                  {/* {
-
-                    this.props.tracks.map(track => {
-                        return <Track track={track} key={track.id} />
-                    })
-                }   */}
+                        {this.props.tracks.map(track => {
+                          return <Track track={track} key={track.id} 
+                          onAdd={this.props.onAdd} onRemove={this.props.onRemove}
+                          isRemoval={this.props.isRemoval}
+                          />})}    
             </div>
         )
     }
